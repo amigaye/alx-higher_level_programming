6e3bcf502018ba4979700c14cc7e66ebb4175ea9
Muluken Kebede <mulerami9@gmail.com>
2022-08-30 14:45:11 -0700

    multiple of two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
 
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-             check_div.append(True)
+            check_div.append(True)
         else:
             check_div.append(False)
 
