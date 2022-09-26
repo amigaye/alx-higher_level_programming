0397838044ca27bd50dfbebcf2d7d558246de2a6
Muluken Kebede <mulerami9@gmail.com>
2022-09-26 14:17:37 -0700

    my int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,8 @@ class MyInt(int):
 
     def __eq__(self, other):
         """ Method that returns != check """
-        
         return int.__ne__(self, other)
 
     def __ne__(self, other):
         """ Method that returns == check """
-
-    return int.__eq__(self, other)
+        return int.__eq__(self, other)
