2f2958e406f1121dd0cd78710d2b08f2eeee4f03
Muluken Kebede <mulerami9@gmail.com>
2022-09-26 14:55:48 -0700

    add atribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""add attribute
+"""
+
 def add_attribute(*args):
+    """ Function that adds a new attribute to an object
+
+    Args:
+        obj: object
+        name: attribute name
+        value: attribute value
+
+    Raises:
+        TypeError: when the attribute can't be added
+
+    """
+
     if "main" in str(type(args[0])):
         setattr(args[0], args[1], args[2])
     else:
