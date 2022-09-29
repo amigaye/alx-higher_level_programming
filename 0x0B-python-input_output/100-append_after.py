4c00f844d2b7d739ae37d5b57cf50850dd570a69
Muluken Kebede <mulerami9@gmail.com>
2022-09-29 10:24:44 -0700

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,7 +9,7 @@ def append_after(filename="", search_string="", new_string=""):
     res_line = []
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
-            res_line +=[line]
+            res_line += [line]
             if line.find(search_string) != -1:
                 res_line += [new_string]
 
