08ba24fc193924e1845b15af9619963c85b945be
Muluken Kebede <mulerami9@gmail.com>
2022-09-29 08:40:48 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,6 @@ load_file = __import__('6-load_from_json_file').load_from_json_file
 my_list = []
 if os.path.exists("add_item.json"):
     my_list = load_file("add_item.json")
-
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
