2041c51b0f9f9628e5e962b6702a0ccf8900ca3f
root <mulerami9@gmail.com>
2022-12-05 12:25:24 +0000

    javascript warm-up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
