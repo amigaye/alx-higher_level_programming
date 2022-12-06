4088c259bb69c6f4b5a61044b25de022196ad331
root <mulerami9@gmail.com>
2022-12-06 12:01:09 +0000

    javascript warm-up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = [ 
+  "C is fun", "Python is cool", "JavaScript is amazing"
+];
+//let text = "";
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
