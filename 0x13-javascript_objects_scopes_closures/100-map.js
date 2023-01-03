e5025f9487c763325fae8b7d56c80468c947e168
amigaye <mulerami9@gmail.com>
2023-01-03 19:35:42 +0000

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const array= require('./100-data').list;
+// script that imports an array and computes a new array
+
+const array = require('./100-data').list;
 
 console.log(array);
 let count = 0;
 const map1 = array.map(function (x) {
   return (x * count++);
 });
-
 console.log(map1);
