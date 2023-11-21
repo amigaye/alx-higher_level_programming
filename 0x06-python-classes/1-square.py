83c89ef87571f251443640328983d66064f57ba6
amigaye <mulerami9@gmail.com>
2023-11-21 12:32:43 -0800

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square
+    """
+    def __init__(self, size):
+        """Initialize method
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
